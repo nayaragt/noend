e632cdcefaa9ff34160abcb4768aa647aa68dfe1
nayaragt <nayaragt@gmail.com>
2017-01-04 18:47:15 -0200

    new index

---
+++ js/default.js
@@ -0,0 +1,12 @@
+$(function() {
+// Definir playlist
+var playlist = [{
+ artist: 'Daft Punk',
+ title: 'Technologic',
+ mp3: 'songs/technologic.mp3'
+}, {
+ artist: 'Daft Punk',
+ title: 'Human After All',
+  mp3: 'songs/human-after-all.mp3'
+}]; 
+});
